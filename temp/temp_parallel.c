3d4c94e953837f79bdbd8f1c1679fdb0bf55ef81
Wei Xin <weixinl@andrew.cmu.edu>
2020-04-25 12:05:04 -0400

    Added timing code

--- temp/temp_parallel.c
+++ temp/temp_parallel.c
@@ -6,7 +6,6 @@
 #include <omp.h>
 
 #include "cycletimer.h"
-//#include "cycletimer.c"
 
 
 int extra(int j, int *temp) {
@@ -20,73 +19,102 @@ int extra(int j, int *temp) {
 }
 
 
-int main()
-{
-    int thread_count = 8; 
-    omp_set_num_threads(thread_count);
-    int i, j, y;
-
-    int num2 = 1000000000; 
-    int *temp2 = malloc(sizeof(int) * num2); 
-    int *temp3 = malloc(sizeof(int) * num2); 
-    for(i = 0; i < num2; i++) {
-        temp2[i] = 133 * i + 123; 
-        temp3[i] = 0; 
-    }
-
-    int num = num2; 
+int main() {
+    
     double startSeconds = currentSeconds();
 
-    // clock_t begin = clock();
+    int num = 1, primes = 0;
+    // int i,j;
+    int limit = 300000;
 
-    int* temp = malloc(sizeof(int) * num); 
-
-    
-    #pragma omp parallel for schedule(static, 128) 
-    for (j = 0; j < num; j++) {
-        // temp[j] = temp2[j] + extra(j, temp3);
-        y = j * 2; 
-
+    #pragma omp parallel for schedule(dynamic) reduction(+ : primes)
+    for (num = 1; num <= limit; num++) { 
+        int i = 2; 
+        while(i <= num) { 
+            if(num % i == 0)
+                break;
+            i++; 
+        }
+        if(i == num)
+            primes++;
     }
-    
 
-    // y = temp[111]; 
     double endSeconds = currentSeconds();
 
-    // clock_t end = clock();
-    // double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-
-    // diff = clock() - start; 
-    // int msec = diff * 1000 / CLOCKS_PER_SEC; 
-
-    // printf("Time taken %d seconds %d milliseconds", msec/1000, msec % 1000); 
     printf("Start Time: %f\n", startSeconds);
     printf("End Time: %f\n", endSeconds);
     printf("Time: %f\n", endSeconds - startSeconds);
-    // printf("Time: %f\n", time_spent);
-    printf("Y: %d\n\n", y);
-
-
-
-    // y = 0; 
-    // double startSeconds1 = currentSeconds();
-    // // clock_t start = clock(), diff; 
-    // // ProcessIntenseFunction(); 
-    // for (i = 0; i < 100000000; i++){
-    //     z = 3 * i;
-    // }
-
-
-    // double endSeconds1 = currentSeconds();
-
-    // printf("Start Time: %f\n", startSeconds1);
-    // printf("End Time: %f\n", endSeconds1);
-    // printf("Time: %f\n", endSeconds1 - startSeconds1);
-    // printf("Y: %d\n", z);
-
 
     return 0;
 }
+
+// int main()
+// {
+//     int thread_count = 8; 
+//     omp_set_num_threads(thread_count);
+//     int i, j, y;
+
+//     int num2 = 1000000000; 
+//     int *temp2 = malloc(sizeof(int) * num2); 
+//     int *temp3 = malloc(sizeof(int) * num2); 
+//     for(i = 0; i < num2; i++) {
+//         temp2[i] = 133 * i + 123; 
+//         temp3[i] = 0; 
+//     }
+
+//     int num = num2; 
+//     double startSeconds = currentSeconds();
+
+//     // clock_t begin = clock();
+
+//     int* temp = malloc(sizeof(int) * num); 
+
+    
+//     #pragma omp parallel for schedule(static, 128) 
+//     for (j = 0; j < num; j++) {
+//         // temp[j] = temp2[j] + extra(j, temp3);
+//         y = j * 2; 
+
+//     }
+    
+
+//     // y = temp[111]; 
+//     double endSeconds = currentSeconds();
+
+//     // clock_t end = clock();
+//     // double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+
+//     // diff = clock() - start; 
+//     // int msec = diff * 1000 / CLOCKS_PER_SEC; 
+
+//     // printf("Time taken %d seconds %d milliseconds", msec/1000, msec % 1000); 
+//     printf("Start Time: %f\n", startSeconds);
+//     printf("End Time: %f\n", endSeconds);
+//     printf("Time: %f\n", endSeconds - startSeconds);
+//     // printf("Time: %f\n", time_spent);
+//     printf("Y: %d\n\n", y);
+
+
+
+//     // y = 0; 
+//     // double startSeconds1 = currentSeconds();
+//     // // clock_t start = clock(), diff; 
+//     // // ProcessIntenseFunction(); 
+//     // for (i = 0; i < 100000000; i++){
+//     //     z = 3 * i;
+//     // }
+
+
+//     // double endSeconds1 = currentSeconds();
+
+//     // printf("Start Time: %f\n", startSeconds1);
+//     // printf("End Time: %f\n", endSeconds1);
+//     // printf("Time: %f\n", endSeconds1 - startSeconds1);
+//     // printf("Y: %d\n", z);
+
+
+//     return 0;
+// }
 
 // /******************************************************************************
 // * FILE: omp_hello.c
